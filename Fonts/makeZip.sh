adcac026e13ffa91786fbf51b4b518d7b4f1b5c2
StevenSYS <139715581+StevenSYS@users.noreply.github.com>
2025-01-17 00:10:24 +0000

    Now you can make your own zip files that have the AuriText font sheets in them!

---
+++ Fonts/makeZip.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+outputDir="Fonts"
+outputZip="Fonts.zip"
+
+if [ -e $outputDir/ ]; then
+	rm -rf $outputDir/
+fi
+mkdir $outputDir
+
+if [ -e $outputZip ]; then
+	rm $outputZip
+fi
+
+echo -e "\"These fonts are both public-domain, feel free to do whatever you want with them.\" - StevenSYS" > "$outputDir/README.txt"
+
+for i in */; do
+	./makeFontSheet.sh "$i" "$outputDir/${i%/}.png"
+done
+
+7za a -mx9 $outputZip $outputDir/
+
+rm -rf $outputDir/
