38c11821f67dcc30354aa349e1e5b97cdfe473c0
Steven <139715581+StevenSYS@users.noreply.github.com>
2025-01-26 04:17:59 +0000

    I need to change `.gitignore` as well, but I'm making this commit on my phone, so I have to split this commit into two.
    (Commit 1/2)

--- Fonts/makeZip.sh
+++ Fonts/makeZip.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
-outputDir="Fonts"
-outputZip="Fonts.zip"
-
-if [ -e $outputDir/ ]; then
-	rm -rf $outputDir/
-fi
-mkdir $outputDir
-
-if [ -e $outputZip ]; then
-	rm $outputZip
+# $1 = Output Zip File (Optional)
+# $2 = Output Directory (Optional)
+# $3 = Info Text File (Optional)
+if [ -e "$1" ]; then
+	outputZip="$1"
+else
+	outputZip="Fonts.zip
 fi
 
-echo -e "\"These fonts are both public-domain, feel free to do whatever you want with them.\" - StevenSYS" > "$outputDir/README.txt"
+if [ -e "$2" ]; then
+	outputDir="$2"
+else
+	outputDir="Fonts"
+fi
+
+if [ -e "$outputDir/" ]; then
+	rm -rf "$outputDir/"
+fi
+mkdir "$outputDir"
+
+if [ -e "$outputZip" ]; then
+	rm "$outputZip"
+fi
+
+if [ -e "$3" ]; then
+	cp "$3" "$outputDir/Info.txt"
+fi
 
 for i in */; do
 	./makeFontSheet.sh "$i" "$outputDir/${i%/}.png"
 done
 
-7za a -mx9 $outputZip $outputDir/
-rm -rf $outputDir/
+7za a -mx9 "$outputZip" "$outputDir/"
+rm -rf "$outputDir/"
